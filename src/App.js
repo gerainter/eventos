6a4ec6b2f284f402c53eb84accfb7fd2a4e5fd74
gerainter <gerainter@gmail.com>
2022-02-25 02:36:29 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,16 +7,27 @@ export default function App() {
   let imagen2 ="https://img.icons8.com/stickers/100/000000/lederhosen.png";
   const valor = 21.0;
   const refCaja = useRef();
-  function incrementar(e){
+  const incrementar=(e)=>{
     e.target.innerHTML=Number(e.target.innerHTML)+1;
+    if(Number(e.target.innerHTML)>7 && Number(e.target.innerHTML)<10){   
+      e.target.style.backgroundColor="red";
+    }else if(Number(e.target.innerHTML)>=10 ){
+      e.target.innerHTML=1;
+      e.target.style.backgroundColor="white";
+    }
   }
-  function calcularCambio(e){
+  const calcularCambio=(e)=>{
     refCaja.current.innerHTML=Number(refCaja.current.innerHTML)*valor;
   }
-  function cambiaImagen(h){
-    h.target.src = imagen2;
+  const cambiaImagen=(h)=>{
+    
+    if(h.target.src.includes(imagen1)){
+      h.target.src = imagen2;
+    }else{
+      h.target.src = imagen1;
+    }
   }
-  function cambiaTexto(r){
+  const cambiaTexto=(r)=>{
     refCaja.current.innerHTML=r.target.value;
   }
   return (
